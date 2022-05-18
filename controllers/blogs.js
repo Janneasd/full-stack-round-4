86cca0f8991c1bcdee96bec000440408262c2c85
Hölttä Janne <janne.s.holtta@aalto.fi>
2022-05-18 21:57:48 +0300

    first commit

---
+++ controllers/blogs.js
@@ -0,0 +1,74 @@
+const { request } = require('../app')
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+
+
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+  .find({})
+  .populate('user', { name: 1, username: 1 })
+  response.json(blogs)
+     
+  })
+
+  blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    if(!body.title || !body.url) {
+      response.status(400).end()
+    }
+  
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user.id
+    })
+  
+   const savedBlog = await blog.save()
+   user.blogs = user.blogs.concat(savedBlog.id)
+   await user.save()
+   response.json(savedBlog.toJSON())
+       
+
+    
+  
+  })
+
+  blogsRouter.delete('/:id', async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    const blog = await Blog.findById(request.params.id)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    
+    else (decodedToken.id === blog.user.toString()) 
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()
+    
+    
+  })
+
+
+  blogsRouter.put('/:id', async (request, response, next) => {
+
+    blogsRouter.put('/:id', async (request, response) => {
+      const blog = request.body
+      await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      response.json(blog.toJSON)
+    })
+  })
+
+ 
+
+  module.exports = blogsRouter
